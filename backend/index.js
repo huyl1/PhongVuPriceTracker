240366ea65d9c11d044d4a1b4b98137cc4fd6093
Huy Le <57906351+huyl1@users.noreply.github.com>
2024-07-26 22:48:43 +0000

    connecting to db

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import { PORT } from './config';
 import mongoose from 'mongoose';
+import fs from 'fs';
 
 const app = express();
 
-app.get('/', (req, res) => {});
+// Connect to MongoDB
+const loginData = fs.readFileSync('./login.json', 'utf8');
+const login = JSON.parse(loginData);
+const mongodbstring = login.mongodbstring;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+// Connect to MongoDB using the mongodbstring
 mongoose
+    .connect(mongodbstring)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
