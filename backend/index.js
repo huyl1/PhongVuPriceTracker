313cce3f81407871ded9b14ca3f67f7e1234aefb
Huy Le <57906351+huyl1@users.noreply.github.com>
2024-07-26 22:36:44 +0000

    refactoring

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import { PORT } from './config';
+import mongoose from 'mongoose';
 
 const app = express();
+
+app.get('/', (req, res) => {});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose
